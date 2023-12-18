6476540fde662c32be298dfab0a780e4e280f118
Br0deNN <theo.broden@gmail.com>
2023-12-18 18:08:37 +0100

    Add project files.

---
+++ Apaxiaaaaaaaaaaaans!/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string s = Console.ReadLine();
+        for (int i = 0; i < s.Length - 1; i++)
+        {
+            if (s[i] != s[i + 1])
+            {
+                Console.Write(s[i]);
+            }
+        }
+        Console.Write(s[s.Length - 1]);
+    }
+}
+
